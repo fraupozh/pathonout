856d88d8237cb5d0049b9398080aa88bebab5305
pozhidae <marina.pozhidaeva@ufz.de>
2024-06-17 21:46:59 +0200

    createsuperuser --no-input

--- build.sh
+++ build.sh
@@ -5,6 +5,9 @@ set -o errexit
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
 
+# Enable setting superuser credentials as environmental variables
+python manage.py createsuperuser --no-input
+
 # Convert static asset files
 python manage.py collectstatic --no-input
 
