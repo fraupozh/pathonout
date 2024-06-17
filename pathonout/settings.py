26b0cba7742dbbd00ea42e42cd93f981e46a5c0d
pozhidae <marina.pozhidaeva@ufz.de>
2024-06-17 18:49:03 +0200

    allowed hosts

--- pathonout/settings.py
+++ pathonout/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'pathonout.onrender.com']
 
 
 # Application definition
