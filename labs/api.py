c73e9a99068e8314eb4e431c6310a74bc4bbf6ac
pozhidae <marina.pozhidaeva@ufz.de>
2024-07-09 13:24:23 +0200

    update api.py with sample

--- labs/api.py
+++ labs/api.py
@@ -1,12 +1,10 @@
 from rest_framework import routers
 
-from labs.viewsets import (
-    MarkerViewSet, IsolateViewSet
-)
+from labs.viewsets import MarkerViewSet, SampleViewSet  # Update the import to SampleViewSet
 
 router = routers.DefaultRouter()
 router.register(r"markers", MarkerViewSet)
-router.register(r"isolates", IsolateViewSet)
+router.register(r"samples", SampleViewSet)  # Update the route to "samples"
 
 urlpatterns = router.urls
 
